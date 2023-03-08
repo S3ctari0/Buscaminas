388454b6de92556cdc75bb5016724aba91e82fcf
Jose Miguel <josem.becerram@utadeo.edu.co>
2023-03-07 20:34:56 -0500

    :c

--- buscaminas.py
+++ buscaminas.py
@@ -10,14 +10,6 @@ class Minesweeper:
         self.rows = rows
         self.cols = cols
         self.num_mines = num_mines
-        for i in range(self.rows):
-            for x in range(self.cols):
-                print("-",end=" ")
-            print()
 
     def initialize_board(self):
-        matriz = [[random.randint(0, 6) for j in range(self.rows)] for i in range(self.cols)]
-        minas = random.sample([numero for fila in matriz for numero in fila], 6)
-        print(minas)
-
-Minesweeper.initialize_board()
+        tablero = [[Cell]]
