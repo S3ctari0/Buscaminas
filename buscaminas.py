4c29f1d02dee1e6e45d0078e63d869e1083b65e4
Jorge Garcia <locodeira1b3@gmail.com>
2023-03-07 20:47:14 -0500

    cambiando cosas

--- buscaminas.py
+++ buscaminas.py
@@ -1,15 +1,35 @@
 import random 
 
+
 class Cell:
     def __init__(self):
         self.is_mine = False
         self.is_revealed = False
         self.adjacent_mine = False
+
+
 class Minesweeper:
     def __init__(self, rows, cols, num_mines):
         self.rows = rows
         self.cols = cols
         self.num_mines = num_mines
+        self.tablero = []
 
     def initialize_board(self):
-        tablero = [[Cell]]
+        self.tablero = [[Cell()]*self.cols for _ in range(self.rows)]
+
+    def __str__(self):
+        todo = ''
+        for line in self.tablero:
+            for element in line:
+                if element.is_mine:
+                    todo += '+'
+                else:
+                    todo += '*'
+            todo += '\n'
+        return todo
+
+
+tablero = Minesweeper(5, 5, 7)
+tablero.initialize_board()
+print(tablero)
