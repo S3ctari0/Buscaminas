8f64d0b4e2c06b1334be79cbd88f690427f87432
Jose Miguel <josem.becerram@utadeo.edu.co>
2023-03-09 21:31:59 -0500

    va paya

--- buscaminas.py
+++ buscaminas.py
@@ -24,24 +24,33 @@ class Minesweeper:
             while self.tablero[columna][fila].is_mine:
                 columna = random.randint(0, self.cols-1)
                 fila = random.randint(0, self.rows-1)
-            print(f"{columna}, {fila}")
             self.tablero[columna][fila].is_mine = True
             for j in range(-1, 2):
                 for k in range(-1, 2):
                     if (columna+j in range(self.cols)) and (fila+k in range(self.rows)): 
                         self.tablero[columna+j][fila+k].adj_mines += 1
+    
+    def reveal_cell(self,row,col):
+        if row in range(self.rows) and col in range(self.cols):
+            self.tablero[col][row].is_revealed == True
 
     def __str__(self):
-        todo = ''
+
+        tablero = ""
         for line in self.tablero:
             for element in line:
+                if element.is_revealed == False:
+                    tablero += ' * '
+                    continue
                 if element.is_mine:
-                    todo += ' + '
+                    tablero += ' + '
                 else:
-                    todo += str(f" {element.adj_mines} ")
-            todo += '\n'
-        return todo
-
-tablero = Minesweeper(5, 5, 7)
+                    tablero += str(f" {element.adj_mines} ")
+            tablero += '\n'
+        return tablero
+    
+tablero = Minesweeper(10, 10, 7)
 tablero.initialize_board()
+print(tablero)
+tablero.reveal_cell(3,4)
 print(tablero)
