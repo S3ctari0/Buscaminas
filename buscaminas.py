57325a6038dd02fd84c8987e908676912f049bbb
Jorge Garcia <locodeira1b3@gmail.com>
2023-03-10 19:30:20 -0500

    Finalizado

--- buscaminas.py
+++ buscaminas.py
@@ -5,7 +5,7 @@ class Cell:
     def __init__(self):
         self.is_mine = False
         self.is_revealed = False
-        self.adj_mines = 0
+        self.adjacent_mines = 0
 
 
 class Minesweeper:
@@ -13,60 +13,88 @@ class Minesweeper:
         self.rows = rows
         self.cols = cols
         self.num_mines = num_mines
-        self.tablero = []
-        self.relevated_cells = 0
-        self.mine_relevated = False
+        self.board = []
+        self.num_revealed = 0
+        self.game_over = False
 
     def initialize_board(self):
-        self.tablero = [[0]*self.cols for _ in range(self.rows)]
+        self.board = [[0]*self.cols for _ in range(self.rows)]
         for i in range(self.cols):
             for j in range(self.rows):
-                self.tablero[i][j] = Cell()
+                self.board[i][j] = Cell()
 
         for i in range(self.num_mines):
-            columna = random.randint(0, self.cols-1)
-            fila = random.randint(0, self.rows-1)
-            while self.tablero[columna][fila].is_mine:
-                columna = random.randint(0, self.cols-1)
-                fila = random.randint(0, self.rows-1)
-            self.tablero[columna][fila].is_mine = True
+            col = random.randint(0, self.cols-1)
+            row = random.randint(0, self.rows-1)
+            while self.board[col][row].is_mine:
+                col = random.randint(0, self.cols-1)
+                row = random.randint(0, self.rows-1)
+            self.board[col][row].is_mine = True
             for j in range(-1, 2):
                 for k in range(-1, 2):
-                    if (columna+j in range(self.cols)) and (fila+k in range(self.rows)):
-                        self.tablero[columna+j][fila+k].adj_mines += 1
+                    if (col+j in range(self.cols)) and (row+k in range(self.rows)):
+                        self.board[col+j][row+k].adjacent_mines += 1
 
     def reveal_cell(self, row,  col):
         if row in range(self.rows) and col in range(self.cols):
-            if self.tablero[col][row].is_revealed:
+            if self.board[col][row].is_revealed:
                 return
-            self.tablero[col][row].is_revealed = True
-            if self.tablero[col][row].adj_mines == 0:
+            self.board[col][row].is_revealed = True
+            self.num_revealed += 1
+            if self.board[col][row].is_mine:
+                self.game_over = True
+                return
+            if self.board[col][row].adjacent_mines == 0:
                 self.reveal_cell(row-1, col)
                 self.reveal_cell(row, col-1)
                 self.reveal_cell(row+1, col)
                 self.reveal_cell(row, col+1)
 
     def __str__(self):
-        tablero = ""
-        for line in self.tablero:
+        board = "  "
+        for i in range(cols):
+            board += f' {i} '
+        board += '\n'
+        for index, line in enumerate(self.board):
+            board += f'{ index} '
             for element in line:
                 if element.is_revealed is False:
-                    tablero += ' * '
+                    board += ' - '
                     continue
                 if element.is_mine:
-                    tablero += ' + '
+                    board += ' X '
                 else:
-                    tablero += str(f" {element.adj_mines} ")
-            tablero += '\n'
-        return tablero
+                    board += str(f" {element.adjacent_mines} ")
+            board += '\n'
+        return board
 
-    def win(self):
-        if self.mine_relevated:
+    def is_win(self):
+        if self.game_over:
             print("Juego Perdido")
+        if self.num_revealed == self.cols*self.rows - self.num_mines:
+            return True
+        return False
 
 
-tablero = Minesweeper(10, 10, 7)
-tablero.initialize_board()
-print(tablero)
-tablero.reveal_cell(3,  4)
-print(tablero)
+rows = int(input("Ingrese el número de filas: "))
+cols = int(input("Ingrese el número de columnas: "))
+mines = int(input("Ingrese el número de minas: "))
+
+
+board = Minesweeper(rows, cols, mines)
+board.initialize_board()
+
+
+while (board.game_over is False or board.is_win()):
+    row = int(input("Ingrese el número de la fila de la casilla a revelar: "))
+    col = int(input("Ingrese el número de la columna de la casilla a revelar: "))
+    board.reveal_cell(row, col)
+    if board.game_over:
+        print("¡Lo siento, has perdido!")
+        print(board)
+        break
+    if board.is_win():
+        print("¡Haz Ganado, felicidades!")
+        print(board)
+        break
+    print(board)
